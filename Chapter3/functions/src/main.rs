57b0441577be2ec0cc307e8a1c2e17bf5d87cf66
krabs <krabs@rustaceosbox>
2023-10-06 04:59:35 +0000

    Functions with Return Values

--- Chapter3/functions/src/main.rs
+++ Chapter3/functions/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    let x = plus_one(5);
 
-    println!("The value of y is: {y}");
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
