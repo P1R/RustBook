bf35febe6dc84fdd3427f2ddbc07221548f875a6
krabs <krabs@rustaceosbox>
2023-10-06 04:47:18 +0000

    Statements and Expressions

--- Chapter3/functions/src/main.rs
+++ Chapter3/functions/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    print_labeled_measurement(5, 'h');
-}
+    let y = {
+        let x = 3;
+        x + 1
+    };
 
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+    println!("The value of y is: {y}");
 }
