358c025730c770845f60f0ff60ff2e50d3695757
krabs <krabs@rustaceosbox>
2023-10-06 04:37:26 +0000

    Parameters

--- Chapter3/functions/src/main.rs
+++ Chapter3/functions/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
