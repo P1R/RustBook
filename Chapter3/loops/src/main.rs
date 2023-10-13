c0fcf71c25aab088661a4e7946efba84095ea669
krabs <krabs@rustaceosbox>
2023-10-13 05:06:19 +0000

    Loop Labels to Disambiguate Between Multiple Loops

--- Chapter3/loops/src/main.rs
+++ Chapter3/loops/src/main.rs
@@ -1,13 +1,21 @@
 fn main() {
-    let mut counter = 0;
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
 
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
         }
-    };
 
-    println!("The result is {result}");
+        count += 1;
+    }
+    println!("End count = {count}");
 }
